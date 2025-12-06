1022446f90b98ef22a2e15c82064944735549e29
RomelashviliDaniel <s31393@pjwstk.edu.pl>
2025-12-06 13:23:01 +0100

    small refactoring of names

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -1,2 +1,73 @@
-package PACKAGE_NAME;public class ClientHandler {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+
+public class ClientHandler implements Runnable {
+    private final Server server;
+    private final Socket socket;
+    private PrintWriter out;
+    private String username;
+
+    public ClientHandler(Server server, Socket socket) {
+        this.server = server;
+        this.socket = socket;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public void run() {
+        try {
+            out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            if (!server.clients.isEmpty()) {
+                out.println("Connected users: " + server.clients.keySet());
+            } else {
+                out.println("no users connected");
+            }
+            out.println("Enter your username:");
+            this.username = in.readLine();
+
+            server.registerlient(username, this);
+            String message;
+
+
+            while ((message = in.readLine()) != null) {
+                if (server.checkMessageForBannedPhrase(message)) {
+                    out.println("Message contains banned content and was not sent.");
+                } else  if (message.equals("Query")) {
+                    out.println(server.bannedWords);
+                } else if (message.contains("exclude")){
+                    out.println("You have excluded the user from sent message");
+                    server.broadcastToAllExceptExcluded(username + ": " + message, this, message);
+                } else if(message.contains("whisper to")) {
+                    server.privateMessage(username + ": " + message, message);
+                }
+                else {
+                    server.broadcastToAll(username + ": " + message, this);
+                }
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }  finally {
+            server.removeClient(username);
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void sendMessage(String message) {
+        out.println(message);
+    }
+
 }
+
--- src/Server.java
+++ src/Server.java
@@ -1,2 +1,94 @@
-package PACKAGE_NAME;public class Server {
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.*;
+
+public class Server {
+    private int port;
+    private String name;
+    Set<String> bannedWords = new HashSet<>();
+    Map<String, ClientHandler> clients = new HashMap<>();
+
+    public Server(String configFile) {
+        loadConfig(configFile);
+
+    }
+
+    private void loadConfig(String configFile) {
+        Properties config = new Properties();
+        try(FileInputStream fileInputStream = new FileInputStream(configFile)) {
+            config.load(fileInputStream);
+            this.port = Integer.parseInt(config.getProperty("Port"));
+            this.name = config.getProperty("Name");
+            String bannedPhrases = config.getProperty("BannedWOrds");
+            if (bannedPhrases != null) {
+                bannedWords.addAll(Arrays.asList(bannedPhrases.split(" ")));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    synchronized void registerlient(String name, ClientHandler clientHandler) {
+        clients.put(name, clientHandler);
+        broadcastToAll("User " + name + " has joined the server.", null);
+
+    }
+    synchronized void removeClient(String name) {
+        clients.remove(name);
+        broadcastToAll("User " + name + " has left the chat.", null);
+
+
+    }
+    synchronized void broadcastToAll(String message,ClientHandler excludeClient) {
+        clients.forEach((username, client) -> {
+            if (client != excludeClient) {
+                client.sendMessage(message);
+            }
+        });
+    }
+    synchronized void broadcastToAllExceptExcluded(String message,ClientHandler excludeClient, String excludedUser) {
+        clients.forEach((username, client) -> {
+            if (client != excludeClient && !excludedUser.contains(username)) {
+                client.sendMessage(message);
+            }
+        });
+    }
+    synchronized void privateMessage(String message, String meantUser) {
+        clients.forEach((username, client) -> {
+            if (meantUser.contains(username)) {
+                client.sendMessage(message);
+            }
+        });
+
+
+    }
+    void start() {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println(name + " server started on port " + port + bannedWords);
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                ClientHandler clientHandler = new ClientHandler(this, clientSocket);
+                new Thread(clientHandler).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean checkMessageForBannedPhrase(String message) {
+        boolean check = false;
+        for(String phrase : bannedWords) {
+            if (message.contains(phrase)) {
+                check = true;
+                break;
+            }
+        }
+        return check;
+    }
+
+
+
+
+
 }
